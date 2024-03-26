9447e809285e255ba85cd3aefb71f6b333811bc0
subhro1530 <shaswata.ssaha@gmail.com>
2024-03-26 17:07:11 +0530

    com 3

--- blog_news_project/settings.py
+++ blog_news_project/settings.py
@@ -35,6 +35,13 @@ DEBUG = True
 ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
+import os
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles_build', 'static'),]
+
 # Application definition
 
 INSTALLED_APPS = [
